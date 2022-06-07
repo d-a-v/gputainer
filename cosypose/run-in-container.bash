022ac3b5adfc62c6793241c8c52318aeac8328da
David Gauchard <gauchard@laas.fr>
2022-06-07 15:13:58 +0200

    print pytorch version

--- cosypose/run-in-container.bash
+++ cosypose/run-in-container.bash
@@ -1,4 +1,6 @@
 
+set +x
+
 . /conda/conda/etc/profile.d/conda.sh
 conda activate /conda/app
 export PATH=/conda/app/bin:${PATH}
@@ -9,17 +11,21 @@ env|grep -i PYTHON
 echo CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}
 
 # vglrun seems to need BOTH '-d :1' and 'DISPLAY=:1' ?
-vglrun -d ${DISPLAY} python -c 'import torch;print("torch.cuda.device_count()=", torch.cuda.device_count(), "\ntorch.cuda.is_available()=", torch.cuda.is_available(), "\ntorch.version.cuda=", torch.version.cuda, "\ntorch.backends.cudnn.version()=", torch.backends.cudnn.version());'
+vglrun -d ${DISPLAY} python -c 'import torch;print("torch.__version__=", torch.__version__, "\ntorch.cuda.device_count()=", torch.cuda.device_count(), "\ntorch.cuda.is_available()=", torch.cuda.is_available(), "\ntorch.version.cuda=", torch.version.cuda, "\ntorch.backends.cudnn.version()=", torch.backends.cudnn.version());'
 
 export CUDA_LAUNCH_BLOCKING=1
 cd cosypose
 
-export PYTHONPATH=$(pwd)/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/bullet3/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/job-runner/build/lib.linux-x86_64-cpython-37:${PYTHONPATH}
-export LD_LIBRARY_PATH=$(pwd)/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/bullet3/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/job-runner/build/lib.linux-x86_64-cpython-37:${LD_LIBRARY_PATH}
-
+# build but not setting up because apptainer's sif file might be not writable
+# then setting PATH below
 [ -d build ] || python setup.py build
 (cd deps/bullet3; [ -d build ] || python setup.py build)
 (cd deps/job-runner; [ -d build ] || python setup.py build)
+export PYTHONPATH=$(pwd)/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/bullet3/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/job-runner/build/lib.linux-x86_64-cpython-37:${PYTHONPATH}
+export LD_LIBRARY_PATH=$(pwd)/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/bullet3/build/lib.linux-x86_64-cpython-37:$(pwd)/deps/job-runner/build/lib.linux-x86_64-cpython-37:${LD_LIBRARY_PATH}
+
+ls -al /
+echo PWD=$(pwd)
 
 vglrun -d ${DISPLAY} python -m cosypose.scripts.run_cosypose_eval --config tless-siso --debug
 #??vglrun -d ${DISPLAY} python -m cosypose.scripts.prediction_script
