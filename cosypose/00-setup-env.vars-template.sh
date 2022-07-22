b29c0ff31dc8ef57f68d3a02bd3c9415edbc55e6
David Gauchard <gauchard@laas.fr>
2022-07-22 11:58:24 +0200

    remove cosypose submodule, add setup variable file

---
+++ cosypose/00-setup-env.vars-template.sh
@@ -0,0 +1,13 @@
+
+set -e
+
+# cosypose repository
+cosypose=https://github.com/ylabbe/cosypose
+
+# conda environment
+# use empty variable to use upstream environment
+#condaenv=""       
+condaenv=condaenv/environment-1.8.1.yaml
+
+# cosypose local_data
+local_data=/net/cubitus/projects/datasets4dl/cosypose/local_data
