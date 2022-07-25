c9da59b9a513dfc8d34b0edc568f81079d8a2032
David Gauchard <gauchard@laas.fr>
2022-07-25 11:22:27 +0200

    ros_cosypose: update setup scripts

---
+++ ros_cosypose/00-setup-env.vars-template.sh
@@ -0,0 +1,13 @@
+
+set -e
+
+# cosypose repository
+cosypose=https://github.com/ylabbe/cosypose
+
+# conda environment
+# use empty variable to use upstream environment
+#condaenv=""       
+condaenv=ros_cosypose/environment.yaml
+
+# cosypose local_data
+local_data=/net/cubitus/projects/datasets4dl/cosypose/local_data
