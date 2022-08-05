8c56038914ed310b40e4a0a4131deb55061b4cb9
David Gauchard <gauchard@laas.fr>
2022-08-05 16:49:05 +0200

    add sifDir-to-sifFile helper script simplify environment

--- cosypose/00-setup-env.vars-template.sh
+++ cosypose/00-setup-env.vars-template.sh
@@ -7,7 +7,8 @@ cosypose=https://github.com/ylabbe/cosypose
 # conda environment
 # use empty variable to use upstream environment
 #condaenv=""       
-condaenv=condaenv/environment-1.8.1.yaml
+#condaenv=condaenv/environment-1.8.1.yaml
+condaenv=condaenv/environment-latest.yaml
 
 # cosypose local_data
 local_data=/net/cubitus/projects/datasets4dl/cosypose/local_data
