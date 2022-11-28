9007dd5b85197aac19b300eebc961c3582225190
Elliot Maitre <emaitre@guidoni.laas.fr>
2022-11-28 13:11:36 +0100

    Removing paths

--- cosypose/custom_training/custom_training/cosypose_config.py
+++ cosypose/custom_training/custom_training/cosypose_config.py
@@ -2,18 +2,13 @@ import os
 from pathlib import Path
 
 # Folder of the dataset images, in which train_pbr folder is located
-# GENERATED_DS_DIR = "blocks_dataset"
-# GENERATED_DS_DIR = "blocks_multi"
-GENERATED_DS_DIR = "tless"
-# GENERATED_DS_DIR = "blocks_uni"
+GENERATED_DS_DIR = # Name of the generated dataset
 
-CUSTOM_DS_DIR = Path(os.environ["SCRATCH"]) / "/net/cubitus/projects/datasets4dl/users/emaitre/cosypose/data/scenes" / GENERATED_DS_DIR
-print("path to scratch", Path(os.environ["SCRATCH"]))
-print("custom ds dir = ", CUSTOM_DS_DIR)
+# Path to custom dataset, to train on
+CUSTOM_DS_DIR = # Path to training data
 DEST_PLY_DIR = CUSTOM_DS_DIR / "models"
 
 # Folder of the URDF models
-# CAD_DATASET_NAME = "cubes_dataset"
-CAD_DATASET_NAME = "tless.cad"
-# CAD_DATASET_NAME = "cubes_dataset_uni"
-CAD_MODELS_DIR = Path("/net/cubitus/projects/datasets4dl/cosypose/local_data/urdfs/") / CAD_DATASET_NAME
+CAD_DATASET_NAME = # Name of the dataset
+# Path to urdf models
+CAD_MODELS_DIR = #path to urdf models / CAD_DATASET_NAME
